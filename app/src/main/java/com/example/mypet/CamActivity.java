8ec21573eb09c9822f962c93e573aea6d73a7bd1
dlsdud01 <minemonang2@gmail.com>
2025-04-16 13:29:15 +0900

    0416

--- app/src/main/java/com/example/mypet/CamActivity.java
+++ app/src/main/java/com/example/mypet/CamActivity.java
@@ -3,10 +3,27 @@ package com.example.mypet;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.niqdev.ipcam.view.IpCamView;
+import com.github.niqdev.ipcam.view.IpCamUri;
+
 public class CamActivity extends AppCompatActivity {
+
+    private IpCamView ipCamView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
+
+        ipCamView = findViewById(R.id.ipCamView);
+
+        IpCamUri uri = new IpCamUri("Camera1", "http://192.168.0.100:8080/?action=stream");
+        ipCamView.start(uri);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ipCamView != null) ipCamView.stopPlayback();
     }
 }
